354bc0ef21d99e68aac7a29254add8cd2fecd16f
S33NU <62537245+S33NU@users.noreply.github.com>
2020-03-23 14:52:45 +0530

    Update database.py
    There may be further changes

--- registration/functions/database.py
+++ registration/functions/database.py
@@ -2,18 +2,32 @@ import mysql.connector
 import logging
 from mysql.connector import errorcode
 from metadata.functions.metadata import connectToDatabase
- 
- 
+
+
 def saveClientPAsswordDB(dataObj):
     try:
         cnx = connectToDatabase()
         mycursor = cnx.cursor()
-        sql = "UPDATE registration SET password = '"+dataObj['password']+"' WHERE phonenumber ={}".format(dataObj['phonenumber'])
-
+        sql = "UPDATE registration SET password = '" + dataObj['password'] + "' WHERE mobile_no ={}".format(
+            dataObj['mobile_no'])
         mycursor.execute(sql)
+        cnx.commit()
+        cnx.close()
+    except Exception as e:
+        logging.error("Error in saving client password " + str(e))
+        raise
+
+
+def saveClientMobileDB(dataObj):
+    try:
+        cnx = connectToDatabase()
+        mycursor = cnx.cursor()
+        sql = "INSERT INTO registration(mobile_no,ip_address,device) VALUES(%s,%s,%s)"
+        val = (dataObj['mobile_no'], dataObj['ip_address'], dataObj['device'])
+        mycursor.execute(sql, val)
 
         cnx.commit()
         cnx.close()
     except Exception as e:
-        logging.error("Error in saving client password "+str(e))
+        logging.error("Error in saving client mobile number in DB " + str(e))
         raise
